5038548a9ad22308d266fc806418d90b48e03f22
Antoine Pouillaude <a.pouillaude@criteo.com>
2017-04-02 18:56:07 +0200

    Starting Journey to The moon problem.

---
+++ journeyToTheMoon.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <vector> 
+#include <list>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <cstdlib>
+#include <cmath>
+#include <cstdio>
+#include <fstream>
+#include <ctime>
+#include <cassert>
+
+using namespace std;
+
+int main() {
+
+    :wqa
+
+}
