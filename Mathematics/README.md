c948504e05c7aa0c07882c5d3a32e28fc26db1a8
MathuxNY-73 <pouillaude.a@gmail.com>
2018-09-13 00:55:21 +0200

    Tries to solve nCr challenge

--- Mathematics/README.md
+++ Mathematics/README.md
@@ -1,2 +1,12 @@
 # Hackerrank Mathematics
 This repository contains all problems I solved from the hacker rank website regarding Mathematics
+
+## nCr
+This is a mathematics challenge from hackerrank. The goal is to print the nth line of the Pascal triangle thus poviding the number of combination of nCr with 0 <= r <= n.  
+I tried to solve this problem using the C language [Problem link](https://www.hackerrank.com/challenges/ncr-table/problem).  
+As of September 12<sup>th</sup>, 2018 :
+- [X] Find a first working solution on the sample input
+- [X] Submitted solution. Got Floating point exception: 8 as the number gets bigger
+- [X] Find a second working solution on the sample input
+- [X] Submitted solution. Got wrong answer
+- [ ] Challenge solved
---
+++ Mathematics/nCr/main.c
@@ -0,0 +1,134 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define wl(n) while(n--)
+#define fl(i,a,b) for(i=a; i<b; ++i)
+
+// Complete the getWays function below.
+int* solve(int n, int* result_count) {
+    long long num = 1;
+    long long den = 1;
+    *result_count = 0;
+    
+    int* res = malloc(sizeof(int) * (n+1));
+    res[0] = num / den;
+    ++(*result_count);
+    
+    int lim = floor(n/2.0);
+    
+    for(int i = 1; i <= lim ; ++i)
+    {
+        //printf("%d\n", INT_MAX);
+        //printf("Hello1 num: %lld\n", num);
+        num = (long long)(num * ((double)(n-i+1)/i)) % 1000000000;
+        //printf("Hello2 num: %lld\n", num);
+
+        //printf("Hello 1\n");
+        res[i] = num;
+
+        //printf("%d: %d\n", i, res[i]);
+        ++(*result_count);
+    }
+    
+    for(int i = lim + 1; i <= n; ++i)
+    {
+        res[i] = res[n - i];
+        ++(*result_count);
+    }
+    
+    return res;
+}
+
+void fastscan_long(long* number)
+{
+    //variable to indicate sign of input number
+    bool negative = false;
+    register int c;
+ 
+    *number = 0;
+ 
+    // extract current character from buffer
+    c = getchar_unlocked();
+    if (c=='-')
+    {
+        // number is negative
+        negative = true;
+ 
+        // extract the next character from the buffer
+        c = getchar_unlocked();
+    }
+ 
+    // Keep on extracting characters if they are integers
+    // i.e ASCII Value lies from '0'(48) to '9' (57)
+    for (; (c>47 && c<58); c=getchar_unlocked())
+        *number = *number *10 + c - 48;
+ 
+    // if scanned input has a negative sign, negate the
+    // value of the input number
+    if (negative)
+        *number *= -1;
+}
+
+void fastscan_int(int* number)
+{
+    //variable to indicate sign of input number
+    bool negative = false;
+    register int c;
+ 
+    *number = 0;
+ 
+    // extract current character from buffer
+    c = getchar_unlocked();
+    if (c=='-')
+    {
+        // number is negative
+        negative = true;
+ 
+        // extract the next character from the buffer
+        c = getchar_unlocked();
+    }
+ 
+    // Keep on extracting characters if they are integers
+    // i.e ASCII Value lies from '0'(48) to '9' (57)
+    for (; (c>47 && c<58); c=getchar_unlocked())
+        *number = *number *10 + c - 48;
+ 
+    // if scanned input has a negative sign, negate the
+    // value of the input number
+    if (negative)
+        *number *= -1;
+}
+
+int main()
+{
+    int t = 0;
+    fastscan_int(&t);
+
+    wl(t)
+    {
+        int n =0;
+        fastscan_int(&n);
+
+        int result_count = 0;
+
+        int* result = solve(n, &result_count);
+
+        for(int i = 0 ; i < result_count ; ++i)
+        {
+            printf("%d ", result[i]);
+        }
+
+        printf("\n");
+
+        free(result);
+    }
+
+    return 0;
+}
